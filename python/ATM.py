cf4e177c79e755aa77e28f6680be911b574b6b7f
Daksh Rawal <38171023+daksh47@users.noreply.github.com>
2023-10-16 12:31:41 +0530

    Add files via upload

---
+++ python/ATM.py
@@ -0,0 +1,6 @@
+#ATM
+hun=int(input("Enter Number Of Hunderd Rupees Note You Want To : "))
+fhun=int(input("Enter Number Of 5 Hunderd Rupees Note You Want To : "))
+thou=int(input("Enter Number Of Thousand Rupees Note You Want To : "))
+ttl = 100*hun + 500*fhun + 1000*thou
+print("Total Money withdrawn",ttl)
---
+++ python/speed.py
@@ -0,0 +1,5 @@
+#calcuate speed
+d=float(input("Enter Total Distance in METERS "))
+t=float(input("Enter Total Time Taken in SECONDS "))
+s=d/t
+print("Speed",s,"m/s")
